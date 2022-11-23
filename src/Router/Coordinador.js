40c684a1ac51551e217cd727ded68a3aeb2a275b
Adriana <adrianascosta9@gmail.com>
2022-11-23 16:05:12 -0300

    exercicio

---
+++ src/Router/Coordinador.js
@@ -0,0 +1,13 @@
+
+export const goToHomePage = (navigate) => {
+    navigate('/')
+}
+
+export const goToProfilePage = (navigate, nome) => {
+    navigate(`/profile/${nome}`)
+}
+
+
+export const goToProfileProduto = (navigate, nome) => {
+    navigate(`/profile/${nome}`)
+}
